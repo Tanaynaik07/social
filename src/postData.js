af6d53398e22d7b41d611eb7958d27d411bf90e1
Aritra Nanda <138552528+AritraNanda@users.noreply.github.com>
2024-04-14 15:47:49 +0530

    Update postData.js
    25 posts added

--- src/postData.js
+++ src/postData.js
@@ -3,16 +3,157 @@
 const postsData = [
     {
       id: 1,
-      profileImage: "profile1.jpg",
+      profileImage: "src/assests/Profilepics/profile1.jpg",
       username: "John Doe",
-      postImage: "post1.jpg",
+      postImage: "src/assests/Posts/post1.jpg",
     },
+    
     {
       id: 2,
-      profileImage: "profile2.jpg",
+      profileImage: "src/assests/Profilepics/profile2.jpg",
+      username: "Ciara William",
+      postImage: "src/assests/Posts/post2.png",
+    }, 
+    {
+      id: 3,
+      profileImage: "src/assests/Profilepics/profile3.webp",
+      username: "Dhriti Negi",
+      postImage: "src/assests/Posts/post3.png",
+    }, 
+    {
+      id: 4,
+      profileImage: "src/assests/Profilepics/profile4.webp",
+      username: "Abinash Gupta",
+      postImage: "src/assests/Posts/post4.webp",
+    }, 
+    {
+      id: 5,
+      profileImage: "src/assests/Profilepics/profile5.jpeg",
       username: "Jane Smith",
-      postImage: "post2.jpg",
+      postImage: "src/assests/Posts/post5.jpg",
+    }, 
+    {
+      id: 6,
+      profileImage: "src/assests/Profilepics/profile6.jpg",
+      username: "Abhijit Roy",
+      postImage: "src/assests/Posts/post6.png",
+    }, 
+    {
+      id: 7,
+      profileImage: "src/assests/Profilepics/profile7.jpg",
+      username: "Nithin Reddy",
+      postImage: "src/assests/Posts/post7.jpg",
+    }, 
+    {
+      id: 8,
+      profileImage: "src/assests/Profilepics/profile8.jpg",
+      username: "Divya S",
+      postImage: "src/assests/Posts/post8.jpg",
+    }, 
+    {
+      id: 9,
+      profileImage: "src/assests/Profilepics/profile9.webp",
+      username: "Prerona Sarkar",
+      postImage: "src/assests/Posts/post9.png",
+    }, 
+    {
+      id: 10,
+      profileImage: "src/assests/Profilepics/profile10.webp",
+      username: "Shilpi Boruah",
+      postImage: "src/assests/Posts/post10.jpg",
+    }, 
+    {
+        id: 11,
+        profileImage: "src/assests/Profilepics/profile1.jpg",
+        username: "John Doe",
+        postImage: "src/assests/Posts/post11.webp",
     },
+      
+      {
+        id: 12,
+        profileImage: "src/assests/Profilepics/profile2.jpg",
+        username: "Ciara William",
+        postImage: "src/assests/Posts/post12.jpg",
+      }, 
+      {
+        id: 13,
+        profileImage: "src/assests/Profilepics/profile3.webp",
+        username: "Dhriti Negi",
+        postImage: "src/assests/Posts/post13.png",
+      }, 
+      {
+        id: 14,
+        profileImage: "src/assests/Profilepics/profile4.webp",
+        username: "Abinash Gupta",
+        postImage: "src/assests/Posts/post14.jpg",
+      }, 
+      {
+        id: 15,
+        profileImage: "src/assests/Profilepics/profile5.jpeg",
+        username: "Jane Smith",
+        postImage: "src/assests/Posts/post15.jpeg",
+      }, 
+      {
+        id: 16,
+        profileImage: "src/assests/Profilepics/profile6.jpg",
+        username: "Abhijit Roy",
+        postImage: "src/assests/Posts/post16.jpg",
+      }, 
+      {
+        id: 17,
+        profileImage: "src/assests/Profilepics/profile7.jpg",
+        username: "Nithin Reddy",
+        postImage: "src/assests/Posts/post17.png",
+      }, 
+      {
+        id: 18,
+        profileImage: "src/assests/Profilepics/profile8.jpg",
+        username: "Divya S",
+        postImage: "src/assests/Posts/post18.jpg",
+      }, 
+      {
+        id: 19,
+        profileImage: "src/assests/Profilepics/profile9.webp",
+        username: "Prerona Sarkar",
+        postImage: "src/assests/Posts/post19.jpg",
+      }, 
+      {
+        id: 20,
+        profileImage: "src/assests/Profilepics/profile10.webp",
+        username: "Shilpi Boruah",
+        postImage: "src/assests/Posts/post20.jpg",
+      },
+      {
+        id: 21,
+        profileImage: "src/assests/Profilepics/profile1.jpg",
+        username: "John Doe",
+        postImage: "src/assests/Posts/post21.webp",
+      },
+      
+      {
+        id: 22,
+        profileImage: "src/assests/Profilepics/profile2.jpg",
+        username: "Ciara William",
+        postImage: "src/assests/Posts/post2.png",
+      }, 
+      {
+        id: 23,
+        profileImage: "src/assests/Profilepics/profile3.webp",
+        username: "Dhriti Negi",
+        postImage: "src/assests/Posts/post23.png",
+      }, 
+      {
+        id: 24,
+        profileImage: "src/assests/Profilepics/profile4.webp",
+        username: "Abinash Gupta",
+        postImage: "src/assests/Posts/post24.jpg",
+      }, 
+      {
+        id: 25,
+        profileImage: "src/assests/Profilepics/profile5.jpeg",
+        username: "Jane Smith",
+        postImage: "src/assests/Posts/post25.webp",
+      },
     // Add more posts as needed
   ];
   
